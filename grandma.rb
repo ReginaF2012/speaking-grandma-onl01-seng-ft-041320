f8c61c0b88e4ad0d279512d5c85c70103f1fcb20
Regina Furness <skritschy@gmail.com>
2020-04-14 01:02:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
-# Write a speak_to_grandma method.
+# Write a speak_to_grandma method
+def speak_to_grandma(string)
+ if string=="I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+else string!=speak_to_grandma(string).upcase
+  puts "HUH!? SPEAK UP, SONNY!"
+end
+end
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
