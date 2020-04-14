8683f9c5dd973d2940b94352abaa2741dcb38809
Regina Furness <skritschy@gmail.com>
2020-04-14 01:06:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,10 @@
 def speak_to_grandma(string)
  if string=="I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-else string!=string.upcase
+elsif string!=string.upcase
   "HUH?! SPEAK UP, SONNY!"
+else 
+  "NO, NOT SINCE 1938!"
 end
 end
 
